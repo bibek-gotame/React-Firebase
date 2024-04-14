5d046f943efc142124b3243d930990d53b6796b1
bibek-gotame <bibekgotame011@gmail.com>
2024-04-14 17:23:13 +0545

    refactored

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -4,20 +4,16 @@ import {
 } from "firebase/auth";
 import { useRef, useState } from "react";
 import { auth } from "../../firebase";
-import { useSelector } from "react-redux";
 import Header from "./Header";
 
 function SignIn() {
   const [isSignIn, setIsSignIn] = useState(true);
-  const [Err, setErr] = useState('')
+  const [Err, setErr] = useState("");
   const name = useRef();
   const email = useRef();
   const password = useRef();
 
-  const user = useSelector((store) => store.user);
-
   const handleSubmit = () => {
-
     if (!isSignIn) {
       createUserWithEmailAndPassword(
         auth,
@@ -30,7 +26,7 @@ function SignIn() {
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
-          setErr(errorCode + errorMessage)
+          setErr(errorCode + errorMessage);
         });
     } else {
       signInWithEmailAndPassword(
@@ -44,76 +40,71 @@ function SignIn() {
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
-          setErr(errorCode + errorMessage)
-    })
+          setErr(errorCode + errorMessage);
+        });
+    }
+  };
 
-  }
-  }
-  // if (user) {
-  //   navigate("/dashBoard");
-  // } else
-    return (
-      <>
+  return (
+    <>
       <div className="bg-slate-500 h-screen">
-     
-          <Header/>
+        <Header />
 
-      <div className=" text-white  bg-black w-[22rem] px-5 py-10 mx-auto mt-36  rounded-lg ">
-        <h1 className=" font-bold text-2xl mb-4">
-          {isSignIn ? " Sign In " : " Sign Up"}
-        </h1>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
+        <div className=" text-white  bg-black w-[22rem] px-5 py-10 mx-auto mt-36  rounded-lg ">
+          <h1 className=" font-bold text-2xl mb-4">
+            {isSignIn ? " Sign In " : " Sign Up"}
+          </h1>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
 
-            // handleSubmit(e)
-          }}
-          className=" text-black  flex flex-col gap-6"
-        >
-          {!isSignIn && (
+              // handleSubmit(e)
+            }}
+            className=" text-black  flex flex-col gap-6"
+          >
+            {!isSignIn && (
+              <input
+                className="rounded-md p-2"
+                ref={name}
+                type="text"
+                placeholder="enter your name"
+              />
+            )}
             <input
               className="rounded-md p-2"
-              ref={name}
-              type="text"
-              placeholder="enter your name"
+              ref={email}
+              type="email"
+              placeholder="email"
             />
-          )}
-          <input
-            className="rounded-md p-2"
-            ref={email}
-            type="email"
-            placeholder="email"
-          />
-          <input
-            className="rounded-md p-2"
-            ref={password}
-            type="text"
-            placeholder="password"
-          />
-<div className="text-red-600">          {Err}
-</div>          <button
-            onClick={handleSubmit}
-            type="submit"
-            className="text-white  bg-red-600 rounded-md py-2 font-bold text-2xl  "
+            <input
+              className="rounded-md p-2"
+              ref={password}
+              type="text"
+              placeholder="password"
+            />
+            <div className="text-red-600"> {Err}</div>{" "}
+            <button
+              onClick={handleSubmit}
+              type="submit"
+              className="text-white  bg-red-600 rounded-md py-2 font-bold text-2xl  "
+            >
+              {isSignIn ? "Sign In" : "Sign Up"}
+            </button>
+          </form>
+          <div
+            onClick={() => {
+              setIsSignIn(!isSignIn);
+            }}
+            className="mt-4 hover:underline cursor-pointer"
           >
-            {isSignIn ? "Sign In" : "Sign Up"}
-          </button>
-        </form>
-        <div
-          onClick={() => {
-            setIsSignIn(!isSignIn);
-          }}
-          className="mt-4 hover:underline cursor-pointer"
-        >
-          {isSignIn
-            ? "Do not have account ? Sign Up "
-            : "Already Have account ? Sign In"}
+            {isSignIn
+              ? "Do not have account ? Sign Up "
+              : "Already Have account ? Sign In"}
+          </div>
         </div>
       </div>
-         
-      </div>
-      </>
-    );
+    </>
+  );
 }
 
 export default SignIn;
